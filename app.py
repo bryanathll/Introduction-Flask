388d4e2683d4fb4bcda2c48ffd9931bec4bbce27
bryanathll <bryannatanael77@gmail.com>
2024-01-29 17:19:13 +0700

    init

--- app.py
+++ app.py
@@ -4,7 +4,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello world"
+    # return "Hello world"
+    return """
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple flask App</title>
+    </head>
+    <body>
+        <h1> Hello world</h1>
+        <P>CANGGIH JUGA anjay</P>
+    </body>
+    </html>
+    """
+    
 
 if __name__ == "__main__":
     app.run()
